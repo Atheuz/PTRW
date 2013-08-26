152601b0521b48f0da8ec3e1411af629a52c386f
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2013-08-26 04:41:44 +0200

    Small tool for pushing torrents to remote watch directory.
    Can push single torrents, directories of torrents or just
    any torrent in the watch directory to remote watch directory,
    entirely up to the user.
    
    Requires a fabricrc with the env.host_string set to the desired
    remote url with username(e.g user@example.com), and the
    env.key_filename set to a private key(e.g [r"private.ppk"].
    Could probably also just have login details here, but I don't know.
    
    Initial commit.

---
+++ main.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Filename      main.py
+# Author        Lasse Vang Gravesen <gravesenlasse@gmail.com>
+# First edited  26-08-2013 04:13
+# Last edited   26-08-2013 04:41
+
+import argparse
+import os
+import shutil
+import glob
+from fabric.api import env
+from fabric.operations import put
+
+def run(files=None):
+    if files is None:
+        files_to_push = glob.glob("watch/*.torrent")
+        for i in files_to_push:
+            put(i, "private/deluge/watch")
+            # Can't have the watch folder keep growing, so once a file has been put it is moved to the archive dir.
+            shutil.move(i, "archive")
+    else:
+        for i in files:
+            if os.path.isfile(i) and os.path.splitext(i)[1] == ".torrent":
+                put(i, "private/deluge/watch")
+                # Don't want to mess with files outside of the watch dir, so we copy instead.
+                shutil.copy(i, "archive")
+            elif os.path.isdir(i):
+                files_to_push = glob.glob(os.path.join(i, "*.torrent"))
+                for j in files_to_push:
+                    put(j, "private/deluge/watch")
+                    shutil.copy(j, "archive")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', metavar='f', type=str, nargs='*',
+                        help='Set file or directory of files to push to watch.')
+    args = parser.parse_args()
+
+    env.host_string = "atheuz@ourea.feralhosting.com"
+    env.key_filename = [r"feralhosting_private.ppk"]
+
+    if args.files:
+        run(files=args.files)
+    else:
+        run()
+
+if __name__ == '__main__':
+    main()
+
